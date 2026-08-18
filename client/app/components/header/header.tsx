2405f3e89052615a8710e8e107dbc42b989867f8
MuhasinBiruar <mxebiruar@addu.edu.ph>
2026-08-19 01:45:30 +0800

    feat: add display role when log in for header

--- client/app/components/header/header.tsx
+++ client/app/components/header/header.tsx
@@ -1,13 +1,24 @@
 "use client";
 
-import { useState } from "react";
-import { Building2, ChevronDown } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Building2 } from "lucide-react";
+import { authClient } from "@/app/lib/auth-client";
 
-const roles = ["Role A", "Role B", "Role C", "Role D"];
 
 export default function Header() {
-  const [role, setRole] = useState(roles[0]);
+  const [role, setRole] = useState("");
   const [open, setOpen] = useState(false);
+
+
+  useEffect(() => {
+
+    authClient.getSession().then(({ data, error }) => {
+      console.log("Session data:", data); //remove later
+      const user = data?.user as unknown as { jobRole?: string } | undefined;
+      setRole(user?.jobRole ?? "Unknown Role");
+    });
+
+  }, []);
 
   return (
     <div className="flex items-start sm:items-center justify-between px-4 sm:px-5 py-3 border-b border-gray-700 gap-2">
@@ -22,29 +33,10 @@ export default function Header() {
           onClick={() => setOpen(!open)}
           className="flex items-center gap-1 text-xs sm:text-sm text-gray-500 cursor-pointer"
         >
-          Role: {role}
-          <ChevronDown
-            size={14}
-            className={`transition-transform ${open ? "rotate-180" : ""}`}
-          />
+          Role: <span>{role}</span>
         </button>
 
-        {open && (
-          <div className="absolute right-0 mt-1 w-32 rounded-md border border-gray-200 bg-white shadow-lg z-10">
-            {roles.map((r) => (
-              <button
-                key={r}
-                onClick={() => {
-                  setRole(r);
-                  setOpen(false);
-                }}
-                className="block w-full text-left px-3 py-2 text-xs sm:text-sm text-gray-600 hover:bg-indigo-50 cursor-pointer"
-              >
-                {r}
-              </button>
-            ))}
-          </div>
-        )}
+
       </div>
     </div>
   );
