a58f585d08c72f81048a71ea614b4f603e3b7d55
cyjico <37214242+cyjico@users.noreply.github.com>
2026-08-18 11:06:46 +0800

    refactor: generalize nullableStringField as withNullDefault

--- server/src/schemas/deceasedrecords.ts
+++ server/src/schemas/deceasedrecords.ts
@@ -1,18 +1,18 @@
 import { z } from 'zod';
-import nullableStringField from './util/nullableStringField.ts';
+import withNullDefault from './util/with-null-default.ts';
 
 export const deceasedrecordsSchema = z.object({
   firstname: z.string().min(1),
-  middlename: nullableStringField(),
+  middlename: withNullDefault(z.string().min(1)),
   lastname: z.string().min(1),
-  causeofdeath: nullableStringField(),
-  typeofdeath: nullableStringField(),
-  physicaldescription: nullableStringField(),
+  causeofdeath: withNullDefault(z.string().min(1)),
+  typeofdeath: withNullDefault(z.string().min(1)),
+  physicaldescription: withNullDefault(z.string().min(1)),
   servicestatus: z.enum(['intake', 'active', 'pending', 'completed']),
   hasmaturedlifeplan: z.boolean(),
   plantype: z.enum(['Direct', 'Life', 'LGU']),
   datecreated: z.coerce.date(),
-  managedby: nullableStringField(),
+  managedby: withNullDefault(z.string().min(1)),
 });
 
 export type DeceasedRecordsSchema = z.infer<typeof deceasedrecordsSchema>;
--- server/src/schemas/documents.ts
+++ server/src/schemas/documents.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod';
-import nullableStringField from './util/nullableStringField.ts';
+import withNullDefault from './util/with-null-default.ts';
 
 export const documentsSchema = z.object({
   documenttype: z.string().min(1),
   verificationstatus: z.enum(['pending', 'verified', 'rejected']),
   uploaddate: z.coerce.date(),
-  verifiedby: nullableStringField(),
+  verifiedby: withNullDefault(z.string().min(1)),
   caseid: z.int(),
 });
 
