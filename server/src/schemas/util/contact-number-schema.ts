26bf8def12ddef0a8d6baafff4e14c61b60eea84
cyjico <37214242+cyjico@users.noreply.github.com>
2026-08-22 15:35:19 +0800

    feat: add contactNumberSchema & nameSchema

---
+++ server/src/schemas/util/contact-number-schema.ts
@@ -0,0 +1,22 @@
+import z from 'zod';
+
+const contactNumberSchema = z
+  .string()
+  .transform((val) => val.replace(/[\s\-()]/g, ''))
+  .pipe(
+    z
+      .string()
+      .regex(
+        /^(09\d{9}|\+[1-9]\d{1,14})$/,
+        'Must be a valid local number (e.g., 09123456789) or international format (e.g., +14155552671)',
+      )
+      .transform((val) => {
+        // 3. Convert clean local PH numbers to +63 format, leave others alone
+        if (val.startsWith('09')) {
+          return '+63' + val.substring(1);
+        }
+        return val;
+      }),
+  );
+
+export default contactNumberSchema;
