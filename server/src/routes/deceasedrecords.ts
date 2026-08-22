10bc0a6b1a1fa03a47353bca23951b86ea863214
cyjico <37214242+cyjico@users.noreply.github.com>
2026-08-22 16:09:29 +0800

    fix: add missing $12 placeholder in deceasedrecords

--- server/src/routes/deceasedrecords.ts
+++ server/src/routes/deceasedrecords.ts
@@ -70,7 +70,7 @@ router.post(
         datecreated,
         managedby,
         representedby
-      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
       RETURNING caseid;`,
         [
           parsed.firstname,
