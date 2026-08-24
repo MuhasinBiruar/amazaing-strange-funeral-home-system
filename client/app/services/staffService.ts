78988101ce1afaee2255fb8f293a19d9cef36e1d
cyjico <37214242+cyjico@users.noreply.github.com>
2026-08-24 19:43:26 +0800

    feat: enhance error handling

--- client/app/services/staffService.ts
+++ client/app/services/staffService.ts
@@ -1,4 +1,4 @@
-import type { StaffSchemaType } from 'shared';
+import type { AppErrorResponse, StaffSchemaType } from 'shared';
 import { API } from './api';
 import axios from 'axios';
 
@@ -12,15 +12,29 @@ export const getStaff = async (username: string) => {
   }
 };
 
+export function extractErrorMessage(data: AppErrorResponse): string {
+  const details = data.error.details;
+
+  // Format array of field-specific errors if available
+  if (Array.isArray(details) && details.length > 0) {
+    const grouped = Map.groupBy(details, (item) => item.field);
+
+    return Array.from(grouped, ([field, items]) => {
+      return `${field}: ${items.map((i) => i.message).join(', ')}`;
+    }).join('; ');
+  }
+
+  return data.error.message;
+}
+
 export const createStaff = async (staffData: StaffSchemaType) => {
   try {
     const res = await API.post('/staff', staffData);
     return res.data;
   } catch (error) {
-    if (axios.isAxiosError(error) && error.response?.data?.error?.message) {
-      // Rethrow with the real message
-      throw new Error(error.response.data.error.message);
-    }
+    if (axios.isAxiosError(error) && error.response?.data?.error?.message)
+      throw new Error(extractErrorMessage(error.response.data));
+
     console.error('Error creating staff:', error);
     throw error;
   }
