f19cb78687fdea7880d19533014c91e3f3284e8d
MuhasinBiruar <mxebiruar@addu.edu.ph>
2026-08-24 21:19:20 +0800

    feat: add log out to header

--- client/app/components/header/header.tsx
+++ client/app/components/header/header.tsx
@@ -1,23 +1,22 @@
-'use client';
+"use client";
 
 import { useEffect, useState } from "react";
 import { Building2 } from "lucide-react";
 import { authClient } from "@/app/lib/auth-client";
-
+import { useRouter } from "next/navigation";
 
 export default function Header() {
   const [role, setRole] = useState("");
   const [open, setOpen] = useState(false);
-
+  const [clickLogOut, setClickLogOut] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
-
     authClient.getSession().then(({ data, error }) => {
       console.log("Session data:", data); //remove later
       const user = data?.user as unknown as { jobRole?: string } | undefined;
       setRole(user?.jobRole ?? "Unknown Role");
     });
-
   }, []);
 
   return (
@@ -28,16 +27,50 @@ export default function Header() {
           Villa Elisa Funeral Home
         </span>
       </div>
-      <div className="relative shrink-0">
+      <div className="relative shrink-0 grip grid-row-2">
         <button
           onClick={() => setOpen(!open)}
-          className="flex items-center gap-1 text-xs sm:text-sm text-gray-500 cursor-pointer"
+          className="flex items-center gap-1 text-xs sm:text-sm text-gray-500 cursor-pointer font-bold"
         >
           Role: <span>{role}</span>
         </button>
-
-
+        <button
+          onClick={() => setClickLogOut(true)}
+          className="relative flex items-center gap-1 text-xs font-bold sm:text-sm text-gray-500 hover:cursor-pointer hover:text-indigo-600 transition-colors duration-500 after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-px after:w-full after:bg-indigo-600 after:scale-x-0 after:origin-left hover:after:scale-x-100 after:transition-transform after:duration-500 after:ease-in-out"
+        >
+          Log Out
+        </button>
       </div>
+      {clickLogOut && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black/60 bg-opacity-50 min-h-screen w-full z-50">
+          <div className="bg-white p-10 rounded-lg shadow-md text-center border-black shadow-black">
+            <h2 className="text-4xl text-[#00236F] font-bold mb-4">
+              Attention
+            </h2>
+            <p className="text-lg text-gray-700">
+              Are you sure you want to log out?
+            </p>
+            <div className="flex justify-center gap-4 mt-6">
+              <button
+                onClick={() => setClickLogOut(false)}
+                className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400 transition-colors duration-500 hover:cursor-pointer"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={() => {
+                  authClient.signOut();
+                  setClickLogOut(false);
+                  router.push("/");
+                }}
+                className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition-colors duration-500 hover:cursor-pointer"
+              >
+                Log Out
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
