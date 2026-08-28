7eabf9b6839ed81b8f2ab26e4e720e08b6b31d80
MuhasinBiruar <mxebiruar@addu.edu.ph>
2026-08-28 20:34:23 +0800

    fix: short timeout

--- client/services/api.ts
+++ client/services/api.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const TIMEOUT_MX = 6000 as const;
+
 export const API = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  timeout: 3000,
+  timeout: TIMEOUT_MX,
   withCredentials: true,
   headers: {
     Accept: 'application/json',
