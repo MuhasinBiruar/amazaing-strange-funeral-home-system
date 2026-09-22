500cf59a628bfbfa43b4dea92ce38fc80473ea8b
cyjico <37214242+cyjico@users.noreply.github.com>
2026-09-22 23:11:21 +0800

    refactor(Header): replace log out confirmation modal with info modal

--- client/components/header/index.tsx
+++ client/components/header/index.tsx
@@ -1,16 +1,17 @@
 'use client';
 
-import { useState } from 'react';
 import { Building2 } from 'lucide-react';
 import { authClient } from '@/lib/auth-client';
 import { useAuth } from '@/contexts/AuthProvider';
 import { useRouter } from 'next/navigation';
+import { useInfoModal } from '@/hooks/useInfoModal';
+import { createPortal } from 'react-dom';
 
 export default function Header() {
-  const { jobRole } = useAuth();
-  const [open, setOpen] = useState(false);
-  const [clickLogOut, setClickLogOut] = useState(false);
   const router = useRouter();
+  const { jobRole } = useAuth();
+
+  const { infoModal, showInfo } = useInfoModal();
 
   return (
     <div className="flex items-center justify-between px-4 sm:px-5 py-3 border-b border-gray-700 gap-2 sticky top-0 bg-white/50 backdrop-blur-md z-50">
@@ -30,54 +31,36 @@ export default function Header() {
       </div>
 
       <div className="relative shrink-0 grip grid-row-2">
-        <button
-          onClick={() => setOpen(!open)}
-          className="flex items-center gap-1 text-xs sm:text-sm text-gray-500 cursor-pointer font-bold"
-        >
+        <p className="flex items-center gap-1 text-xs sm:text-sm text-gray-500 font-bold">
           Role: <span>{jobRole ?? 'Unknown Role'}</span>
-        </button>
+        </p>
         <button
-          onClick={() => setClickLogOut(true)}
+          onClick={async () => {
+            const isConfirmed = await showInfo({
+              title: 'Log Out?',
+              message: 'Are you sure you want to log out?',
+              closeLabel: 'Cancel',
+              confirmLabel: 'Log Out',
+              severity: 'warning',
+            });
+
+            if (!isConfirmed) return;
+
+            authClient.signOut({
+              fetchOptions: {
+                onSuccess: () => {
+                  router.push('/');
+                },
+              },
+            });
+          }}
           className="relative flex items-center gap-1 text-xs font-bold sm:text-sm text-gray-500 hover:cursor-pointer hover:text-indigo-600 transition-colors duration-500 after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-px after:w-full after:bg-indigo-600 after:scale-x-0 after:origin-left hover:after:scale-x-100 after:transition-transform after:duration-500 after:ease-in-out"
         >
           Log Out
         </button>
       </div>
-      {clickLogOut && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black/60 bg-opacity-50 min-h-screen w-full z-50">
-          <div className="bg-white p-10 rounded-lg shadow-md text-center border-black shadow-black">
-            <h2 className="text-4xl text-[#00236F] font-bold mb-4">
-              Attention
-            </h2>
-            <p className="text-lg text-gray-700">
-              Are you sure you want to log out?
-            </p>
-            <div className="flex justify-center gap-4 mt-6">
-              <button
-                onClick={() => setClickLogOut(false)}
-                className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400 transition-colors duration-500 hover:cursor-pointer"
-              >
-                Cancel
-              </button>
-              <button
-                onClick={() => {
-                  authClient.signOut({
-                    fetchOptions: {
-                      onSuccess: () => {
-                        setClickLogOut(false);
-                        router.push('/');
-                      },
-                    },
-                  });
-                }}
-                className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition-colors duration-500 hover:cursor-pointer"
-              >
-                Log Out
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
+
+      {createPortal(infoModal, document.body)}
     </div>
   );
 }
