c03904d3cff3a83de77c41ee4c763c49a5245335
cyjico <37214242+cyjico@users.noreply.github.com>
2026-09-22 18:28:06 +0800

    refactor: make inlineError generic

--- client/components/inlineError.tsx
+++ client/components/inlineError.tsx
@@ -1,11 +1,4 @@
-import type { ValueOf } from 'shared/utils';
-import type { AdminValidationErrors } from '../app/dashboard/admin/_components/staffPanel/types';
-
-export default function InlineError({
-  error,
-}: {
-  error: ValueOf<AdminValidationErrors>;
-}) {
+export default function InlineError({ error }: { error: string }) {
   return (
     <>
       {error && <p className="text-red-700 text-xs font-bold mt-1">{error}</p>}
