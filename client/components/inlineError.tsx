940b9e440e4e1264581cc0e3a16ff128e0f657db
cyjico <37214242+cyjico@users.noreply.github.com>
2026-09-22 19:19:40 +0800

    fix(InlineError): make error prop optional

--- client/components/inlineError.tsx
+++ client/components/inlineError.tsx
@@ -1,4 +1,4 @@
-export default function InlineError({ error }: { error: string }) {
+export default function InlineError({ error }: { error?: string }) {
   return (
     <>
       {error && <p className="text-red-700 text-xs font-bold mt-1">{error}</p>}
